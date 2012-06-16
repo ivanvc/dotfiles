4191a852dd476533a444eeaea9d43fb36ad1d20f
Ivan Valdes (@ivanvc) <iv@nvald.es>
2012-06-15 22:32:51 -0500

    Add Ruby aliases

---
+++ ruby/aliases.zsh
@@ -0,0 +1,10 @@
+# lazy rake alias :P
+alias rk='rake'
+
+# Rails 2.x
+alias sc='script/console'
+alias ss='script/server'
+alias sg='script/generate'
+alias sd='script/destroy'
+alias dbr='rake db:drop db:create db:migrate db:test:clone'
+alias dbc='script/dbconsole'
