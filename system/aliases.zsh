93556c80b8e7283d92ed1e001848bb5fe46de0dc
Ivan Valdes (@ivanvc) <iv@nvald.es>
2012-06-15 22:33:04 -0500

    Add system-wide aliases

---
+++ system/aliases.zsh
@@ -0,0 +1,16 @@
+# cd
+alias ..='cd ..'
+
+# ls
+alias ls="ls -F -G"
+alias l="ls -lAh -G"
+alias ll="ls -l -G"
+alias la='ls -A -G'
+
+# commands starting with % for pasting from web
+alias %=' '
+
+# colors
+alias grep='grep --color=auto'
+alias egrep='grep --color=auto'
+alias fgrep='grep --color=auto'
