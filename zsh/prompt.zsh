53d0573c581c0bb8e894207aacdd1ca74d6dcb0c
Ivan Valdes (@ivanvc) <ivan@ooyala.com>
2012-06-21 15:22:26 -0500

    Fix prompt :P

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,8 @@
-git_branch() {
+function git_branch {
   echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
-rbenv_info() {
+function rbenv_info {
   if which rbenv > /dev/null ; then
     rbenv version | cut -d' ' -f1
   fi
