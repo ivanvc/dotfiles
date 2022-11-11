d9dcefff7cdf59ed854fc1bb49bf136246e8881f
Ivan Valdes <iv@a.ki>
2022-11-10 21:01:29 -0800

    Move bindings to inputrc

--- bash/.bash.vimode.sh
+++ bash/.bash.vimode.sh
@@ -2,12 +2,7 @@
 set -o vi
 
 # But still, use emacs keys to move around
-bind '\C-a':beginning-of-line
-bind '\C-b':backward-char
-bind '\C-e':end-of-line
-bind '\C-f':forward-char
-bind '\C-l':clear-screen
-bind '\C-n':next-history
-bind '\C-p':previous-history
-
 bind -m vi-insert '"\C-x\C-e":edit-and-execute-command'
+
+stty werase undef
+bind '\C-w':unix-word-rubout
