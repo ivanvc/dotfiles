c02a8a15c9091f78c8201d6436af39a7d5755de1
Ivan Valdes <iv@a.ki>
2022-11-10 20:19:17 -0800

    Add clear screen bind

--- bash/.bash.vimode.sh
+++ bash/.bash.vimode.sh
@@ -6,6 +6,8 @@ bind '\C-a':beginning-of-line
 bind '\C-b':backward-char
 bind '\C-e':end-of-line
 bind '\C-f':forward-char
-bind '\C-p':previous-history
+bind '\C-l':clear-screen
 bind '\C-n':next-history
+bind '\C-p':previous-history
+
 bind -m vi-insert '"\C-x\C-e":edit-and-execute-command'
