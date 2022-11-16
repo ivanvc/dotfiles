a79417143ec67aeae99578af8271db53e74b4e40
Ivan Valdes <iv@a.ki>
2022-11-16 10:27:04 -0800

    Set unlimited history

--- _bash/.bashrc.d/bash-history-config.sh
+++ _bash/.bashrc.d/bash-history-config.sh
@@ -4,8 +4,12 @@ shopt -s histverify
 # When the shell exits, append to the history file instead of overwriting it
 shopt -s histappend
 
-# Avoid duplicates
-HISTCONTROL=ignoredups:erasedups
+# Avoid duplicates, ignore commands that start with space
+HISTCONTROL=erasedups:ignoreboth
 
 # After each command, append to the history file and reread it
 PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+
+# Unlimited history
+HISTFILESIZE=
+HISTSIZE=
