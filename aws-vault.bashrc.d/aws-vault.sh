ccec5f7031be5716b9a08a2231d0cc2993ef62a4
Ivan Valdes <iv@a.ki>
2022-11-16 15:52:14 -0800

    Add aws-vault's ave

---
+++ aws-vault.bashrc.d/aws-vault.sh
@@ -0,0 +1,8 @@
+ave() {
+  [ -n "$AWS_VAULT" ] && unset AWS_VAULT
+  eval $(aws-vault exec -d3h "$1" env | grep AWS_ | xargs -I{} echo export {})
+}
+
+unave() {
+  eval $(env | grep AWS_ | xargs -I{} echo unset {})
+}
