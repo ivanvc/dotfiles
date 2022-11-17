4e84e602299974e6185b6f08164048d8f1eac9a0
Ivan Valdes <iv@a.ki>
2022-11-17 15:55:03 -0800

    Update bash prompt, add git branch

--- _bash/.bashrc.d/bash-prompt.sh
+++ _bash/.bashrc.d/bash-prompt.sh
@@ -1,1 +1,6 @@
-PS1='${PWD/$HOME/"~"}\[\e[38;2;127;127;170m\]❯\[\033[00m\] '
+_prompt_git_branch() {
+  branch=$(git symbolic-ref HEAD 2>/dev/null | awk -F/ '{print $NF}')
+  [ ! -z "$branch" ] && printf "\e[1m\e[38;2;98;114;164m@\033[0m$branch"
+}
+
+PS1='${PWD/$HOME/"~"}$(_prompt_git_branch)\[\e[38;2;127;127;170m\]❯\[\033[00m\] '
