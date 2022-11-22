64ae1f72b218846fb8e214492165d462fc3b82b7
Ivan Valdes <iv@a.ki>
2022-11-22 11:43:05 -0800

    Update kubectl

--- kubernetes.bashrc.d/kubectl.sh
+++ kubernetes.bashrc.d/kubectl.sh
@@ -1,1 +1,9 @@
+# Alias k to kubectl
 alias k=kubectl
+
+# Load completion
+if command kubectl &> /dev/null; then
+  source <(kubectl completion bash)
+  # Auto complete for the k alias too
+  complete -o default -F __start_kubectl k
+fi
