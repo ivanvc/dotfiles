b0134386d9fe82d8bd4835b08c8d306c7c61e2e6
Ivan Valdes <iv@a.ki>
2022-12-01 17:18:25 -0800

    Autocomplete kc and kn

--- kubernetes.bashrc.d/kubectx.sh
+++ kubernetes.bashrc.d/kubectx.sh
@@ -3,3 +3,9 @@ export KUBECTX_IGNORE_FZF=1
 
 alias kc=kubectx
 alias kn=kubens
+
+if command asdf >/dev/null && [ -d `asdf where kubectx`/completion ]; then
+  source `asdf where kubectx`/completion/*.bash
+  complete -F _kube_contexts kc
+  complete -F _kube_namespaces kn
+fi
