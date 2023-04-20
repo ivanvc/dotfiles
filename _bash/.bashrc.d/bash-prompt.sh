b5e1d60ca58dd870418824d5f7b5d4b44791df5d
Ivan Valdes <iv@a.ki>
2023-04-20 15:39:27 -0700

    Fix escape characters from bash prompt

--- _bash/.bashrc.d/bash-prompt.sh
+++ _bash/.bashrc.d/bash-prompt.sh
@@ -1,6 +1,6 @@
 _prompt_git_branch() {
   branch=$(git symbolic-ref HEAD 2>/dev/null | awk -F/ '{print $NF}')
-  [ ! -z "$branch" ] && printf "\e[1m\e[38;2;98;114;164m@\033[0m$branch"
+  [ ! -z "$branch" ] && echo "@$branch"
 }
 
-PS1='${PWD/$HOME/"~"}$(_prompt_git_branch)\[\e[38;2;127;127;170m\]❯\[\033[00m\] '
+PS1='${PWD/$HOME/"~"}$(_prompt_git_branch|sed s/@/\[\e[01m\]@\[\033[0m\]/)\[\e[38;2;127;127;170m\]❯\[\033[00m\] '
