f2ecabee20348488fd90b80d95bba4aba41f3151
Ivan Valdes <iv@a.ki>
2023-06-23 22:07:28 -0700

    Better bash history

--- _bash/.bashrc.d/bash-history-config.sh
+++ _bash/.bashrc.d/bash-history-config.sh
@@ -5,11 +5,18 @@ shopt -s histverify
 shopt -s histappend
 
 # Avoid duplicates, ignore commands that start with space
-HISTCONTROL=erasedups:ignoreboth
+export HISTCONTROL=erasedups:ignoreboth
 
 # After each command, append to the history file and reread it
 PROMPT_COMMAND="history -a;history -c;history -r;${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}"
 
 # Unlimited history
-export HISTFILESIZE=
-export HISTSIZE=
+export HISTFILESIZE=-1
+export HISTSIZE=-1
+
+# Add timestamp to history
+export HISTTIMEFORMAT="[%F %T] "
+
+# Change history file to a different file to avoid truncation from other
+# instances of bash that may read or write it.
+export HISTFILE=~/.bash_eternal_history
