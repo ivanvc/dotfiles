2ded48f4e22ddd9a95d004699b00a4a7ec97d2ac
Ivan Valdes <iv@a.ki>
2023-06-26 16:04:05 -0700

    Improve ave

--- aws-vault.bashrc.d/aws-vault.sh
+++ aws-vault.bashrc.d/aws-vault.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 ave() {
+  local account="$1"
+  shift
+  local duration="$1"
+  [ -z "$duration" ] && duration=3h
+
   [ -n "$AWS_VAULT" ] && unset AWS_VAULT
-  eval $(aws-vault exec -d3h "$1" env | grep AWS_ | xargs -I{} echo export {})
+  eval "$(aws-vault exec -d"$duration" "$account" env | grep AWS_ | xargs -I{} echo export {})"
 }
 
 unave() {
-  eval $(env | grep AWS_ | xargs -I{} echo unset {})
+  eval "$(env | grep AWS_ | xargs -I{} echo unset {})"
 }
