18ceea2e0ee0054ee0230c0055d6f4737788600d
Ivan Valdes <iv@a.ki>
2023-06-26 15:03:53 -0700

    Support alternative asdf installation path

--- asdf.bashrc.d/asdf.sh
+++ asdf.bashrc.d/asdf.sh
@@ -5,3 +5,7 @@ export ASDF_DATA_DIR=$HOME/.local/share/asdf
 if [ -f /opt/asdf-vm/asdf.sh ]; then
   . /opt/asdf-vm/asdf.sh
 fi
+
+if [ -f /usr/local/opt/asdf/libexec/asdf.sh ]; then
+  . /usr/local/opt/asdf/libexec/asdf.sh
+fi
