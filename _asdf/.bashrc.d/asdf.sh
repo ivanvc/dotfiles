f5d7329f49a8d74214519ed7b6c1e19b9e77d624
Ivan Valdes <iv@a.ki>
2023-07-06 06:34:16 +0000

    Include linuxbrew in asdf paths

--- _asdf/.bashrc.d/asdf.sh
+++ _asdf/.bashrc.d/asdf.sh
@@ -5,7 +5,7 @@ export ASDF_DATA_DIR=$HOME/.local/share/asdf
 
 # Initialize asdf
 __initialize_asdf() {
-  local files="/opt/asdf-vm/asdf.sh /usr/local/opt/asdf/libexec/asdf.sh"
+  local files="/opt/asdf-vm/asdf.sh /usr/local/opt/asdf/libexec/asdf.sh /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh"
   for file in $files; do
     if [ -f "$file" ]; then
       # shellcheck source=/dev/null
