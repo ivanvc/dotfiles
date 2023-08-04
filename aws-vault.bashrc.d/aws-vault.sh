02cf3fb688896fcee47ad35cf799fa5926d8366f
Ivan Valdes <iv@a.ki>
2023-08-04 13:51:35 -0700

    fix: use single '=' for zsh compatibilty

--- aws-vault.bashrc.d/aws-vault.sh
+++ aws-vault.bashrc.d/aws-vault.sh
@@ -2,7 +2,7 @@
 # vim: set noexpandtab:
 
 ave() {
-	if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "help" ]; then
+	if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "help" ]; then
 		cat <<- EOF
 		Usage: ave profile [duration]
 		
@@ -18,7 +18,7 @@ ave() {
 	local duration="$1"
 	[ -z "$duration" ] && duration=3h
 	local macos
-	[ "$(uname -s)" == "Darwin" ] && macos="yes"
+	[ "$(uname -s)" = "Darwin" ] && macos="yes"
 
 	[ -n "$AWS_VAULT" ] && unset AWS_VAULT
 
@@ -27,7 +27,7 @@ ave() {
 
 unave() {
 	local macos
-	[ "$(uname -s)" == "Darwin" ] && macos="yes"
+	[ "$(uname -s)" = "Darwin" ] && macos="yes"
 
 	eval "$(env | grep AWS_ | xargs -I{} ${macos:+"-S1024"} echo unset {})"
 }
