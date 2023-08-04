06b455c2dd1a6390626b3e7af796279d8604c2d9
Ivan Valdes <iv@a.ki>
2023-08-04 10:21:20 -0700

    feat: make ave compatible with macos

--- aws-vault.bashrc.d/aws-vault.sh
+++ aws-vault.bashrc.d/aws-vault.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+# vim: set noexpandtab:
 
 ave() {
-  local account="$1"
-  shift
-  local duration="$1"
-  [ -z "$duration" ] && duration=3h
+	if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "help" ]; then
+		cat <<- EOF
+		Usage: ave profile [duration]
+		
+		profile:  The name of the profile in your ~/.aws/config file to assume.
+		duration: The desired duration for the temporary keys before they expire,
+		          i.e., 5m, 1h, 24h (default: 3h).
+		EOF
+		return
+	fi
 
-  [ -n "$AWS_VAULT" ] && unset AWS_VAULT
-  eval "$(aws-vault exec -d"$duration" "$account" env | grep AWS_ | xargs -I{} echo export {})"
+	local profile="$1"
+	shift
+	local duration="$1"
+	[ -z "$duration" ] && duration=3h
+	local macos
+	[ "$(uname -s)" == "Darwin" ] && macos="yes"
+
+	[ -n "$AWS_VAULT" ] && unset AWS_VAULT
+
+	echo eval "$(aws-vault exec -d"$duration" "$profile" env | grep AWS_ | xargs -I{} ${macos:+"-S1024"} echo export {})"
 }
 
 unave() {
-  eval "$(env | grep AWS_ | xargs -I{} echo unset {})"
+	local macos
+	[ "$(uname -s)" == "Darwin" ] && macos="yes"
+
+	eval "$(env | grep AWS_ | xargs -I{} ${macos:+"-S1024"} echo unset {})"
 }
