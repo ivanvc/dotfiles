020c9a9a041bde12964c46b9a06ef65668635287
Ivan Valdes <iv@a.ki>
2025-02-06 13:57:28 -0800

    feat(asdf): update asdf to 0.16.0

--- _asdf/.bashrc.d/asdf.sh
+++ _asdf/.bashrc.d/asdf.sh
@@ -2,16 +2,4 @@
 
 # Set data directory to ~/.local/share
 export ASDF_DATA_DIR=$HOME/.local/share/asdf
-
-# Initialize asdf
-__initialize_asdf() {
-  local files="/opt/asdf-vm/asdf.sh /usr/local/opt/asdf/libexec/asdf.sh /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh"
-  for file in $files; do
-    if [ -f "$file" ]; then
-      # shellcheck source=/dev/null
-      . "$file"
-    fi
-  done
-}
-
-__initialize_asdf
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
