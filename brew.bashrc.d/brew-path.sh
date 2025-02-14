c548672a5ad8fcf4c3b32edb3f4d91492410ea0e
Ivan Valdes <iv@a.ki>
2025-02-13 22:56:05 -0800

    ci(brew): silence shellcheck errors
    These variables are not being defined in that file.

--- brew.bashrc.d/brew-path.sh
+++ brew.bashrc.d/brew-path.sh
@@ -2,7 +2,10 @@
 
 if [ -d "/home/linuxbrew/.linuxbrew" ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  export LDFLAGS="-L$(brew --prefix)/lib -Wl,-rpath,$(brew --prefix)/lib"
-  export CPPFLAGS="-I$(brew --prefix)/include"
-  export CFLAGS="-I$(brew --prefix)/include"
+  # shellcheck disable=SC2155 # These variables are not being defined here.
+  export LDFLAGS="-L$(brew --prefix)/lib -Wl,-rpath,$(brew --prefix)/lib ${LDFLAGS}"
+  # shellcheck disable=SC2155
+  export CPPFLAGS="-I$(brew --prefix)/include ${CPPFLAGS}"
+  # shellcheck disable=SC2155
+  export CFLAGS="-I$(brew --prefix)/include ${CFLAGS}"
 fi
