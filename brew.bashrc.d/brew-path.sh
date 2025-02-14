75eca8344eaa516cef09756f4d6f285fff7b4359
Ivan Valdes <iv@a.ki>
2025-02-13 16:06:21 -0800

    fix(brew): add library environment variables

--- brew.bashrc.d/brew-path.sh
+++ brew.bashrc.d/brew-path.sh
@@ -2,4 +2,7 @@
 
 if [ -d "/home/linuxbrew/.linuxbrew" ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  export LDFLAGS="-L$(brew --prefix)/lib -Wl,-rpath,$(brew --prefix)/lib"
+  export CPPFLAGS="-I$(brew --prefix)/include"
+  export CFLAGS="-I$(brew --prefix)/include"
 fi
