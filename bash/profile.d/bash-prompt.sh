54bcaed2929c35724924e4b94e6933373982bc3a
Ivan Valdes <iv@a.ki>
2025-02-16 01:13:16 -0800

    feat(bash): update prompt

--- bash/profile.d/bash-prompt.sh
+++ bash/profile.d/bash-prompt.sh
@@ -1,13 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 _prompt_git_branch() {
   branch=$(git symbolic-ref HEAD 2>/dev/null | awk -F/ '{print $NF}')
   commit=$(git rev-parse HEAD 2>/dev/null | awk '{print substr($0,1,7)}')
   if [ -n "$branch" ]; then
-    echo "@$branch"
+    printf "\e[01m@\033[0m$branch"
   elif [ -n "$commit" ]; then
-    echo "#$commit"
+    printf "\e[01m#\033[0m$commit"
   fi
 }
 
-PS1='${PWD/$HOME/"~"}$(_prompt_git_branch|sed -r "s/(@|#)/\[\e[01m\]\1\[\033[0m\]/")\[\e[38;2;127;127;170m\]❯\[\033[00m\] '
+_prompt() {
+  local exit_code=$?
+  if [ -z "${COPY_MODE+x}" ]; then
+    if [ "$exit_code" -eq 0 ]; then
+      printf "${PWD/$HOME/"~"}$(_prompt_git_branch)\e[38;2;127;127;170m❯\033[0m"
+    else
+      printf "${PWD/$HOME/"~"}$(_prompt_git_branch)\e[1;35;170m❯\033[0m"
+    fi
+  else
+    printf "\$"
+  fi
+}
+
+cm() {
+  if [ -z "${COPY_MODE+x}" ]; then
+    export COPY_MODE=1
+  else
+    unset COPY_MODE
+    export COPY_MODE
+  fi
+}
+
+PS1='$(_prompt) '
