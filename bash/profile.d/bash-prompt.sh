57b7a5664d159ba6ac10f428b74c6e738676f131
Ivan Valdes <iv@a.ki>
2025-02-26 22:23:40 -0800

    fix(bash): address shellcheck warnings

--- bash/profile.d/bash-prompt.sh
+++ bash/profile.d/bash-prompt.sh
@@ -15,8 +15,9 @@ _prompt_git_branch() {
 
 __prompt_command() {
   if [ -z "${COPY_MODE+x}" ]; then
-    local job_count=$(jobs -s | wc -l | tr -d 0)
-    if [ ! -z "$job_count" ]; then
+    local job_count
+    job_count=$(jobs -s | wc -l | tr -d 0)
+    if [ -n "$job_count" ]; then
       job_count="\001\033[2;35m\002${job_count}\001\033[5;34m\002❮\001\033[m\002"
     fi
     local exit_code=$?
