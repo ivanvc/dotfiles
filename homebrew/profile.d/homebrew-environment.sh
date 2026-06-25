ced6b616b6c81c6988b9837020459d54cc191c3a
Ivan Valdes <iv@a.ki>
2026-06-24 21:57:52 -0700

    fix(homebrew): fix environment variable order

--- homebrew/profile.d/homebrew-environment.sh
+++ homebrew/profile.d/homebrew-environment.sh
@@ -3,10 +3,11 @@
 function __homebrew_load_env {
   local brew_prefix
   brew_prefix=$(brew --prefix)
-  if [ -d "/home/linuxbrew/.linuxbrew" ]; then
-    export HOMEBREW_BUNDLE_FILE="${XDG_CONFIG_HOME}/homebrew/Brewfile"
-    export BREWFILE="${HOMEBREW_BUNDLE_FILE}"
 
+  export HOMEBREW_BUNDLE_FILE="${XDG_CONFIG_HOME}/homebrew/Brewfile"
+  export BREWFILE="${HOMEBREW_BUNDLE_FILE}"
+
+  if [ -d "/home/linuxbrew/.linuxbrew" ]; then
     # Compilation environment variables
     export LDFLAGS="-L${brew_prefix}/lib -Wl,-rpath,${brew_prefix}/lib ${LDFLAGS}"
     export CPPFLAGS="-I${brew_prefix}/include ${CPPFLAGS}"
