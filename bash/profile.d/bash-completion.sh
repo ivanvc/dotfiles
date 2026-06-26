750954c92e2fcfd71852128ab73b7ac98b451dfe
Ivan Valdes <iv@a.ki>
2026-06-26 15:55:47 -0700

    fix(bash): fix completion

--- bash/profile.d/bash-completion.sh
+++ bash/profile.d/bash-completion.sh
@@ -6,7 +6,7 @@ __load_bash_completion() {
     "/opt/homebrew"
   )
   for path in "${paths[@]}"; do
-    local full_path="${path}/etcd/profile.d/bash_completion.sh"
+    local full_path="${path}/etc/profile.d/bash_completion.sh"
     if [ -r "$full_path" ]; then
       # shellcheck disable=SC1091
       # disable linting the sourced file.
--- homebrew/profile.d/homebrew-completion.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d /home/linuxbrew/.linuxbrew/etc/bash_completion.d ]; then
-  for file in /home/linuxbrew/.linuxbrew/etc/bash_completion.d/*; do
-    # shellcheck source=/dev/null
-    [[ -e "$file" ]] && . "$file"
-  done
-fi
